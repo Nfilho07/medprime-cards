7ecf80910168ffb34b122a83b755ee3443ec20f1
MedPrime.oficial <nelsonfilho583@gmail.com>
2025-08-09 00:27:55 -0300

    Update Layout.jsx

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -27,7 +27,7 @@ import { motion } from "framer-motion";
 import Footer from '../components/medprime/Footer';
 import UpdateChecker from '../components/medprime/UpdateChecker';
 import { NetworkProvider, useNetwork } from '../components/network/NetworkErrorHandler.js';
-import { NetworkInterceptor } from '../components/network/NetworkInterceptor.js';
+import { NetworkInterceptor } from '../components/network/NetworkInterceptor.jsx';
 import { getLevelForXp, getXpForLevel } from '@/components/gamification/utils';
 
 const navigationItems = [
